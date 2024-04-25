dadc676811f2acf3242154b2b02d1f95c8189511
Jefferson Schittini <jefferschi@gmail.com>
2024-04-25 18:18:29 -0300

    primeiros models

--- geesto/models.py
+++ geesto/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+class Fabricante(models.Model):
+    fabricante = models.CharField(max_length=100, verbose_name='Fabricante')    
+    ativo = models.BooleanField(default=True)
+
+    def __str__(self) -> str:
+        return self.fabricante
+        
+class Linha(models.Model):
+    linha = models.CharField(max_length=100, verbose_name='Linha')    
+    ativo = models.BooleanField(default=True)
+
+    def __str__(self) -> str:
+        return self.linha
+    
+
+class Categoria(models.Model):
+    categoria = models.CharField(max_length=100, verbose_name='Categoria')    
+    ativo = models.BooleanField(default=True)
+
+    def __str__(self) -> str:
+        return self.categoria
+
+
+class Produto(models.Model):
+    produto = models.CharField(max_length=100, verbose_name='Produto')
+    linha = models.ForeignKey(Linha, verbose_name='Linha', on_delete= models.PROTECT)
+    categoria = models.ForeignKey(Categoria, verbose_name='Categoria', on_delete= models.PROTECT)
+    fabricante = models.ForeignKey(Fabricante, verbose_name='Fabricante', on_delete= models.PROTECT)
+    ativo = models.BooleanField(default=True)
+
+    def __str__(self) -> str:
+        return self.produto
+    
